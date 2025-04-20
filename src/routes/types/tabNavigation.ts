53dc8bc5a85e8330bf1b9c195f0f9e3e74fa0d33
arouniyar <arouniyar@novaentertainment.com.au>
2025-04-20 19:13:31 +1000

    complete nested nav

--- src/routes/types/tabNavigation.ts
+++ src/routes/types/tabNavigation.ts
@@ -1,10 +1,10 @@
 import { BottomTabParamList, RootStackParamList } from "@/types";
 import { BottomTabNavigationProp } from "@react-navigation/bottom-tabs";
-import { TAB_NAMES } from "../Routes";
+import { TAB_NAMES } from "../Routes"; // Ensure TAB_NAMES is exported as an enum or object
 import { CompositeNavigationProp } from "@react-navigation/native";
 import { StackNavigationProp } from "@react-navigation/stack";
 
 export type DashboardNavProp = CompositeNavigationProp<
-  BottomTabNavigationProp<BottomTabParamList, "Dashboard">,
+  BottomTabNavigationProp<BottomTabParamList, typeof TAB_NAMES.DASHBOARD>,
   StackNavigationProp<RootStackParamList>
 >;
--- src/screens/tabs/DashboardScreen.tsx
+++ src/screens/tabs/DashboardScreen.tsx
@@ -1,19 +1,24 @@
 import { ThemedText } from "@/components";
 import { DashboardNavProp } from "@/routes";
 import { useNavigation } from "@react-navigation/native";
-import React, { FC } from "react";
+import React, { FC, useCallback } from "react";
 import { StyleSheet, Text, View } from "react-native";
 
 type Props = {};
 
 export const DashboardScreen: FC<Props> = ({}) => {
   const { navigate } = useNavigation<DashboardNavProp>();
+
+  const handleOnPress = useCallback(() => {
+    navigate("fullScreen", { name: "MENU 123" });
+  }, [navigate]);
+
   return (
     <View style={styles.conatainer}>
       <ThemedText size="xxLarge" font="bold">
         DashboardScreen
       </ThemedText>
-      <Text>Open Drawer</Text>
+      <Text onPress={handleOnPress}>Open FullScreen</Text>
     </View>
   );
 };
