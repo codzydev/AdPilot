3936df7f60cc1084cd93e2370d728b8d08aff49c
arouniyar <arouniyar@novaentertainment.com.au>
2025-04-26 21:51:24 +1000

    product view

--- src/types/navigation/rootStack.ts
+++ src/types/navigation/rootStack.ts
@@ -5,5 +5,6 @@ import { BottomTabParamList } from "./tabNavigation";
 export type RootStackParamList = {
   [ROOT_STACK]: NavigatorScreenParams<BottomTabParamList>;
   [ROUTES.FULL_SCREEN]: { name: string };
+  [ROUTES.PRODUCT_VIEW_SCREEN]: { id: string };
   [AUTH_STACK]: undefined;
 };
